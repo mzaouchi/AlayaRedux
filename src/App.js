6a4d4bcffdda6999fe3335cff2c1e9a8c3ba2cdd
mzaouchi <mzaouchi@gmail.com>
2023-05-15 15:32:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ListTasks from './Components/ListTasks';
+import AddTask from './Components/AddTask';
+
+function App() {
+  return (
+    <div >
+   <ListTasks/>
+   <AddTask/>
+
+
+    </div>
+  );
+}
+
+export default App;
